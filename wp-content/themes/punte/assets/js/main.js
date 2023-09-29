897238a08215caf7c46b069cbba8bab7e87112a4
Pratik Padaliya <team2@netclues.com>
2023-09-29 13:55:32 +0530

    commit code
    commit code

--- wp-content/themes/punte/assets/js/main.js
+++ wp-content/themes/punte/assets/js/main.js
@@ -121,7 +121,14 @@ jQuery(function ($) {
     } else {
         onpageOffset = 0
     }
-console.log(punte_options.enable_plx_menu);
+
+    //toggle search
+
+    $(".menu-item-search").on("click",function(){
+        $(".site-header .search-form").slideToggle();
+        $(this).toggleClass("ico-close");
+    });
+
 if(punte_options.enable_plx_menu == true ){
     /*START SMOOTH SCROLL JS*/
     $('#site-navigation').onePageNav({
